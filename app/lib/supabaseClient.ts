b81c7c1789818e53c4d5bbb4b74a1b31911b4859
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2024-09-11 22:35:20 +0900

    Set Supabase environment variables

---
+++ app/lib/supabaseClient.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// 環境変数が存在しない場合にエラーメッセージを出す
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL または API Key が設定されていません");
+  }
+  
+  // Supabaseクライアントを作成
+  export const supabase = createClient(supabaseUrl, supabaseAnonKey);
