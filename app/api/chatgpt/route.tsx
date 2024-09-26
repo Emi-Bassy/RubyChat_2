bf8de7b29ef6ffb4073061732e05add18b1be0e1
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2024-09-26 12:22:09 +0900

    rewrite Pool to use Supabase

--- app/api/chatgpt/route.tsx
+++ app/api/chatgpt/route.tsx
@@ -11,10 +11,14 @@ const openai = new OpenAI({
 });
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // Vercelの環境変数から取得
     ssl: {
         rejectUnauthorized: false,
     },
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
 });
 
 export async function POST(req: Request) {
