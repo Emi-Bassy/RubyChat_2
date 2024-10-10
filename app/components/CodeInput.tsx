35c3a77b9cd2bb39e925667fb460f775627f914d
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2024-10-10 10:52:41 +0900

    replace textarea to ace editor

--- app/components/CodeInput.tsx
+++ app/components/CodeInput.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools"
 
 interface CodeInputProps {
     userCode: string;
@@ -8,12 +13,21 @@ interface CodeInputProps {
 const CodeInput: React.FC<CodeInputProps> = ({ userCode, setUserCode }) => {
     return (
         <div className="mt-4">
-            <textarea
-                value={userCode}
-                onChange={(e) => setUserCode(e.target.value)}
-                placeholder="ここにコードを貼り付けてください"
-                className="w-full h-48 p-2 border rounded-md"
-            />
+        <AceEditor
+            mode="ruby"                  // 言語モードをRubyに設定
+            theme="XCode"               // エディタのテーマを設定
+            name="code-editor"            // エディタのID
+            onChange={setUserCode}        // コードが変更されたときに呼び出される関数
+            fontSize={14}                 // フォントサイズの設定
+            value={userCode}              // 現在のコード
+            width="100%"                  // 幅を設定
+            height="250px"                // 高さを設定
+            setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                showLineNumbers: true,
+            }}
+        />
         </div>
     );
 };
