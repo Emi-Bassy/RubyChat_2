d3ae423a583c415d8f8530b0b5365d8b1f7c2dbb
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2024-11-02 06:47:21 +0900

    check

--- app/api/execute/route.ts
+++ app/api/execute/route.ts
@@ -1,17 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { DefaultRubyVM } from "@ruby/wasm-wasi/dist/node";
+import * as fs from "fs";
+import path from "path";
 
 export async function POST(request: NextRequest) {
     // GET /api/users リクエストの処理
-    const url = request.url;
-    const origin = new URL(url).origin;
+    const filePath = path.join(process.cwd(), 'public', 'ruby.wasm');
+    const wasmBuffer = await fs.promises.readFile(filePath);
+
+    const arrayBuffer = wasmBuffer.buffer.slice(wasmBuffer.byteOffset, wasmBuffer.byteOffset + wasmBuffer.byteLength);
+
+    // WebAssembly.compile()にArrayBufferを渡す
+    const module = await WebAssembly.compile(arrayBuffer);
+
+    // const url = request.url;
+    // const origin = new URL(url).origin;
     let result = '';
     const params = await request.json();
     const script = params.script
-    const response = await fetch(`https://ruby-chat-2-m14yuhbur-emi-bassys-projects.vercel.app/ruby.wasm`); // ローカルのWASMファイルを指定
-    const wasmArrayBuffer = await response.arrayBuffer();
-    console.log(wasmArrayBuffer);
-    const module = await WebAssembly.compile(wasmArrayBuffer);
+    // const response = await fetch(`https://ruby-chat-2-m14yuhbur-emi-bassys-projects.vercel.app/ruby.wasm`); // ローカルのWASMファイルを指定
+    // const wasmArrayBuffer = await response.arrayBuffer();
+    // console.log(wasmArrayBuffer);
+    // const module = await WebAssembly.compile(wasmArrayBuffer);
     const rubyVm = await DefaultRubyVM(module);
     // rubyVm.vm.eval(`eval(${script})`)
     try {
